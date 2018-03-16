05de7c79d2bceef6134b641d05efc6c59fb66287
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2018-03-16 11:22:32 -0700

    Add todo class.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
